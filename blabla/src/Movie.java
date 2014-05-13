419b47e82812f8227fca75f822b0e42b9423f765
FK <fabian-kunzke@web.de>
2014-05-13 12:07:36 +0200

    new methods in Movie

--- blabla/src/Movie.java
+++ blabla/src/Movie.java
@@ -6,14 +6,27 @@ public class Movie {
     private int priceCode;
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        priceCode = newpriceCode;
+        setPriceCode(newpriceCode);
     }
     public int getPriceCode() {
-        return priceCode;
+        return _price.getPriceCode();
     }
     public void setPriceCode(int arg) {
-        priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
+    private Price _price;
     public String getTitle (){
         return title;
     };
@@ -42,5 +55,23 @@ public class Movie {
             frequentRenterPoints ++;
         frequentRenterPoints++;
         return frequentRenterPoints;
+    }
+    abstract class Price {
+        abstract int getPriceCode();
+    }
+    class ChildrensPrice extends Price {
+        int getPriceCode() {
+            return Movie.CHILDRENS;
+        }
+    }
+    class NewReleasePrice extends Price {
+        int getPriceCode() {
+            return Movie.NEW_RELEASE;
+        }
+    }
+    class RegularPrice extends Price {
+        int getPriceCode() {
+            return Movie.REGULAR;
+        }
     }
 }
