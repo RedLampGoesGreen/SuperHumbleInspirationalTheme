5547947c5e36b61f4cbfb77fbf5b09e29314fc10
FK <fabian-kunzke@web.de>
2014-05-13 11:39:35 +0200

    bugfixes

--- Rental.java
+++ Rental.java
@@ -13,28 +13,29 @@ class Rental {
     }
     public double getCharge(){
         double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
+        switch (this.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
+                if (this.getDaysRented() > 2)
+                    result += (this.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
+                result += this.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
                 result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
+                if (this.getDaysRented() > 3)
+                    result += (this.getDaysRented() - 3) * 1.5;
                 break;
-            return result;
         }
+        return result;
     }
    public double getFrequentRenterPoints(){
        int frequentRenterPoints=0;
-       if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+       if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1)
            frequentRenterPoints ++;
        frequentRenterPoints++;
+       return frequentRenterPoints;
    }
 
 }
