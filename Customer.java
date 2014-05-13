4433ba5a19906887058e8336329d65cfa2a8eca1
FK <fabian-kunzke@web.de>
2014-05-13 11:00:18 +0200

    methode amountFor moved to Rental

--- Customer.java
+++ Customer.java
@@ -25,8 +25,8 @@ class Customer {
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
-            // new method amountFor sets value fr thisAmount
-            thisAmount = amountFor(each);
+            // new method .getCharge in class Rental
+            thisAmount = each.getCharge();
 
             }
             // add frequent renter points
@@ -43,24 +43,6 @@ class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
-    private double amountFor(Rental aRental){}
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-            return result;
-    }
-    
+       
 }
     
