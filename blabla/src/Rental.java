38994787081c04eade6e04b3d6ef323154626b48
FK <fabian-kunzke@web.de>
2014-05-13 11:55:06 +0200

    moved getCharged to Movie

--- blabla/src/Rental.java
+++ blabla/src/Rental.java
@@ -11,25 +11,10 @@ class Rental {
     public Movie getMovie() {
         return movie;
     }
-    public double getCharge(){
-        double result = 0;
-        switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (this.getDaysRented() > 2)
-                    result += (this.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += this.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (this.getDaysRented() > 3)
-                    result += (this.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+    double getCharge() {
+        return movie.getCharge(daysRented);
     }
+
    public double getFrequentRenterPoints(){
        int frequentRenterPoints=0;
        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1)
